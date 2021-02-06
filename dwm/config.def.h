bbbbfdd2ef6fd1b0fe6b3a6b1fde11bbef61accb
Gilberto <calderongilberto3@gmail.com>
2021-02-06 01:39:52 -0300

    update

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -15,13 +15,17 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "UbuntuMono Nerd Font:size=10:weight=bold:antialias=true:autohint=true" };
+static const char *fonts[]          = { 
+	"UbuntuMono Nerd Font:size=10:weight=bold:antialias=true:autohint=true",
+  "Font Awesome 5 Free:size:10:antialias=true:autohint=true",
+  "JoyPixels:size=10:antialias=true:autohint=true"
+};
 static const char dmenufont[]       = "UbuntuMono Nerd Font:size=10:antialias=true:autohint=true";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#21496C";
+static const char col_cyan[]        = "#00B3FF";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -29,7 +33,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = {"1", "2", "3", "4", "5", "6", "7", "8 ", "9" };
+static const char *tags[] = {"1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
