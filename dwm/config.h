5ad580955122d2612876c8f70ae2366f0ced5322
Gilberto <calderongilberto3@gmail.com>
2021-02-06 21:14:38 -0300

    change awesome bar key hidden This reverts commit c32327c9818096bfd5b3ae86d55d7066021aae41.

--- dwm/config.h
+++ dwm/config.h
@@ -25,7 +25,7 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#1F1F1F";
+static const char col_cyan[]        = "#00B3FF";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -105,7 +105,7 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
 	{ MODKEY,                       XK_s,      show,           {0} },
-	{ MODKEY,                       XK_u,      hide,           {0} },
+	{ MODKEY,                       XK_h,      hide,           {0} },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
